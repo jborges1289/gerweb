3de7325eaa55fa6c7c55001219431cc264335e45
Jeans Michell <licona.dorantes@gmail.com>
2013-11-18 15:27:34 -0600

    Registro
    se edito el form para quitar palabras en ingles

--- protected/views/usuario/_form.php
+++ protected/views/usuario/_form.php
@@ -6,59 +6,68 @@
 
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'usuario-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
-	'enableAjaxValidation'=>false,
-)); ?>
+    <?php
+    $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'usuario-form',
+        // Please note: When you enable ajax validation, make sure the corresponding
+        // controller action is handling ajax validation correctly.
+        // There is a call to performAjaxValidation() commented in generated controller code.
+        // See class documentation of CActiveForm for details on this.
+        'enableAjaxValidation' => false,
+    ));
+    ?>
 
-	<p class="note">Campos con <span class="required">*</span> son necesarios.</p>
+    <p class="note">Campos con <span class="required">*</span> son necesarios.</p>
 
-	<?php echo $form->errorSummary($model); ?>
+    <?php  echo $form->errorSummary($model); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'usuario'); ?>
-		<?php echo $form->textField($model,'usuario',array('size'=>45,'maxlength'=>45)); ?>
-		<?php echo $form->error($model,'usuario'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'usuario'); ?>
+        <?php echo $form->textField($model, 'usuario', array('size' => 45, 'maxlength' => 45)); ?>
+        <?php echo $form->error($model, 'usuario'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'contrasena'); ?>
-		<?php echo $form->textField($model,'contrasena',array('size'=>45,'maxlength'=>45)); ?>
-		<?php echo $form->error($model,'contrasena'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'contrasena'); ?>
+        <?php echo $form->passwordField($model, 'contrasena', array('size' => 45, 'maxlength' => 45)); ?>
+        <?php echo $form->error($model, 'contrasena'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'nombres'); ?>
-		<?php echo $form->textField($model,'nombres',array('size'=>45,'maxlength'=>45)); ?>
-		<?php echo $form->error($model,'nombres'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'nombres'); ?>
+        <?php echo $form->textField($model, 'nombres', array('size' => 45, 'maxlength' => 45)); ?>
+        <?php echo $form->error($model, 'nombres'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'primer_apellido'); ?>
-		<?php echo $form->textField($model,'primer_apellido',array('size'=>45,'maxlength'=>45)); ?>
-		<?php echo $form->error($model,'primer_apellido'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'primer_apellido'); ?>
+        <?php echo $form->textField($model, 'primer_apellido', array('size' => 45, 'maxlength' => 45)); ?>
+        <?php echo $form->error($model, 'primer_apellido'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'segundo_apellido'); ?>
-		<?php echo $form->textField($model,'segundo_apellido',array('size'=>45,'maxlength'=>45)); ?>
-		<?php echo $form->error($model,'segundo_apellido'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'segundo_apellido'); ?>
+        <?php echo $form->textField($model, 'segundo_apellido', array('size' => 45, 'maxlength' => 45)); ?>
+        <?php echo $form->error($model, 'segundo_apellido'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'discriminador'); ?>
-		<?php echo $form->textField($model,'discriminador'); ?>
-		<?php echo $form->error($model,'discriminador'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'discriminador'); ?>
+        <?php //echo $form->textField($model,'discriminador'); ?>
+        <?php
+        echo $form->dropDownList($model, 'discriminador',
+                array('1'=>'Administrador de Proyectos',
+                    '2'=>'Administrador de Riesgos','3'=>'Equipo de Riesgos'),
+                array('empty' => 'Seleccione Perfil'));
+        ?>
+        <?php echo $form->error($model, 'discriminador');
+        ?>
+    </div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
-	</div>
+    <div class="row buttons">
+        <?php echo CHtml::submitButton($model->isNewRecord ? 'Registrar' : 'Editar'); ?>
+    </div>
 
-<?php $this->endWidget(); ?>
+    <?php $this->endWidget(); ?>
 
 </div><!-- form -->
