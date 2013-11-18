3a672c4fa600ab94bdf9249e52c7fd17d7f9d354
Jeans Michell <licona.dorantes@gmail.com>
2013-11-18 15:28:06 -0600

    menu
    se edito el menu para mostrar solo laparte de admin usuarios

--- protected/views/usuario/create.php
+++ protected/views/usuario/create.php
@@ -8,7 +8,6 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'Lista de Usuario', 'url'=>array('index')),
 	array('label'=>'Administrar Usuarios', 'url'=>array('admin')),
 );
 ?>
