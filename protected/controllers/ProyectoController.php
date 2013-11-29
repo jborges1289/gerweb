d1e1055c596cd7ea1ee72554eb6df89e3365b6bb
jborges1289 <jborgescen@gmail.com>
2013-11-29 14:42:55 -0600

    Permisos de proyecto y riesgos Controller listos

--- protected/controllers/ProyectoController.php
+++ protected/controllers/ProyectoController.php
@@ -48,16 +48,16 @@ foreach ($admon_proy as $value) {
 
             
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
+//			array('allow',  // allow all users to perform 'index' and 'view' actions
+//				'actions'=>array('index','view'),
+//				'users'=>array('*'),
+//			),
+//			array('allow', // allow authenticated user to perform 'create' and 'update' actions
+//				'actions'=>array('create','update'),
+//				'users'=>array('@'),
+//			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete', 'gerweb'),
+				'actions'=>array('admin','delete', 'gerweb','create','update','view','index'),
 				'users'=> $usuario,
 //                                'roles'=> array('Administrador de Proyectos'),
 			),
